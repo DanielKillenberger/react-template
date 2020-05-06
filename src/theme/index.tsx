5c15984a357669e1ccee9f02fef59f1141281c18
Daniel Killenberger <daniel.killenberger@gmail.com>
2020-05-06 10:51:00 +0200

    Initial commit with styled-system and styled-components

---
+++ src/theme/index.tsx
@@ -0,0 +1,6 @@
+import {Theme} from 'styled-system';
+import {colors} from './colors';
+import {space} from './space';
+export const breakpoints: string[] = ['319px', '424px', '767px', '1023px'];
+
+
